b49d052ec34fc61a6479724cbbf0cfcc43d2f3cf
Mike Morris <michael.m.morris@pearson.com>
2014-04-24 13:12:36 -0500

    fixed bad package name in kitchen tests

--- test/integration/remove/serverspec/remove_spec.rb
+++ test/integration/remove/serverspec/remove_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-%w(http crypto-utils httpd-manual mod_perl mod_ssl webalizer).each do |p|
+%w(httpd crypto-utils httpd-manual mod_perl mod_ssl webalizer).each do |p|
   describe package p do
     it { should_not be_installed }
   end
