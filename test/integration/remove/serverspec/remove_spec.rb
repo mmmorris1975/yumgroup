80b170fd8123e77bc41d576be7120e955e4dc0d9
Mike Morris <michael.m.morris@pearson.com>
2015-08-18 13:53:34 -0500

    updated spec_helper to comply with new serverspec syntax, added explicit Centos7 package list to serverspec tests

--- test/integration/remove/serverspec/remove_spec.rb
+++ test/integration/remove/serverspec/remove_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
-%w(httpd crypto-utils httpd-manual mod_perl mod_ssl webalizer).each do |p|
+if os[:release].to_i < 7
+  pkgs = %w(httpd crypto-utils httpd-manual mod_perl mod_ssl webalizer)
+else
+  pkgs = %w(httpd crypto-utils httpd-manual mod_ssl)
+end
+
+pkgs.each do |p|
   describe package p do
     it { should_not be_installed }
   end
