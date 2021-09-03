79329607675ec49b22dd2a87ca79b03c9dd95692
Mike <mike@beavis.Home>
2021-09-03 12:00:33 -0500

    update metadata.rb for new release

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Mike Morris'
 maintainer_email 'michael.m.morris@gmail.com'
 license 'BSD-3-Clause'
 description 'Installs/Configures yum packages via yum groups'
-version '0.6.0'
+version '0.7.0'
 
 supports 'redhat', '>= 5.0'
 supports 'centos', '>= 5.0'
